d9b64222b268dffb7ea9b03f47a3fa35d9cd465d
linheri <linhhlinhh11298@gmail.com>
2019-02-22 23:47:30 +0700

    ok

--- inc/content-home.php
+++ inc/content-home.php
@@ -24,14 +24,16 @@
 						</section>
 						<!--Section Teacher-->
 						<section>
-							<h3>MỘT SỐ GIẢNG VIÊN</h3>
-							<hr>
+							<div class="title">
+								<h3>MỘT SỐ GIẢNG VIÊN</h3>
+								<hr>
+							</div>
 							<div class="panel panel-info">
                 <div class="panel-heading clearfix d-none d-sm-block">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-                      <div class="media-body">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
+                      <div class="media-body contentTeacher">
 												<p class="text-primary"><span><strong>Dana (Dane) L. Alden, PhD.</strong></p>
-												<ul>
+												<ul class="listTeacher">
                           <li>Professor of Marketing</li>
                           <li>Chair and William R. Johnson Jr. Distinguished Professor of Marketing</li>
                           <li>Faculty Director, Health Care Management EMBA Program</li>
@@ -42,7 +44,7 @@
 											</div>
                 </div>
                 <div class="panel-heading clearfix d-block d-sm-none">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
                       <div>
 												<p class="text-primary"><span><strong>Dana (Dane) L. Alden, PhD.</strong></p>
                         <ul>
@@ -58,7 +60,7 @@
 						</div>
 						<div class="panel panel-info">
                 <div class="panel-heading clearfix d-none d-sm-block">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
                       <div class="media-body contentTeacher">
 												<p class="text-primary"><span><strong>David Bell, Ph.D.</strong></p>
 												<ul class="listTeacher">
@@ -70,7 +72,7 @@
 											</div>
                 </div>
                 <div class="panel-heading clearfix d-block d-sm-none">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
                       <div class="">
 												<p class="text-primary"><span><strong>David Bell, Ph.D.</strong></p>
                         <ul>
@@ -84,7 +86,7 @@
             </div>
 						<div class="panel panel-info">
                 <div class="panel-heading clearfix d-none d-sm-block">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
                       <div class="media-body contentTeacher">
 												<p class="text-primary"><span><strong>Tung Bui, Ph.D.</strong></p>
 												<ul class="listTeacher">
@@ -97,7 +99,7 @@
 											</div>
                 </div>
                 <div class="panel-heading clearfix d-block d-sm-none">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
                       <div class="">
 												<p class="text-primary"><span><strong>Tung Bui, Ph.D.</strong></p>
                         <ul>
@@ -112,7 +114,7 @@
             </div>
 						<div class="panel panel-info">
                 <div class="panel-heading clearfix d-none d-sm-block">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
                       <div class="media-body contentTeacher">
 												<p class="text-primary"><span><strong>John Butler, Ph.D.</strong></p>
 												<ul class="listTeacher">
@@ -124,7 +126,7 @@
 											</div>
                 </div>
                 <div class="panel-heading clearfix d-block d-sm-none">
-                      <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
+                      <div><img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra"></div>
                       <div class="">
 												<p class="text-primary"><span><strong>John Butler, Ph.D.</strong></p>
                         <ul>
@@ -413,15 +415,18 @@
 						</section>
 						<!--Section Student-->
 						<section>
-            <h3>CẢM NHẬN CỦA CỰU HỌC VIÊN</h3>
-            <hr>
+            <div class="title">
+							<h3>CẢM NHẬN CỦA CỰU HỌC VIÊN</h3>
+							<hr>
+						</div>
             <div class="panel panel-info">
               <div class="panel-heading clearfix d-none d-sm-block">
                     <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-                    <div class="media-body">
-                      <p class="text-primary"><span><strong>Maily Nguyen</strong><br></span>West Coast Implementation Team Head, HSBC, Seattle<br>Class of 2010 – HCMC<br></p>
+                    <div class="media-body contentStudent">
                       <p class="text-muted"><span>“The University of Hawaii Executive MBA program has brought me to a new energy level at which I feel adequate to search for more answers to my questions and face-off more challenges. The program has also given me the opportunity to know one of the most elite groups of professionals and call them friends. Together we have created beautiful memories and wonderful bonds. These are some of the best presents that life can bring.”</span></p>
-                    </div>
+                      <p class="text-primary text-right"><span><strong>Maily Nguyen</strong><br></span>West Coast Implementation Team Head, HSBC, Seattle<br>Class of 2010 – HCMC<br></p>
+
+										</div>
               </div>
               <div class="panel-heading clearfix d-block d-sm-none">
                 <img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
@@ -434,25 +439,28 @@
 						<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Nguyen T. Tra My</strong><br></span>Founder, Chairwoman/CEO of<br>CSC Vietnam, Co-Founder/Vice<br>Chairwoman of The PAN Group.<br>Class of 2007 – Hanoi<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“Why pursuing a quality MBA program overseas while it’s available in Vietnam? With the AACSB accredited Executive MBA program being offered by the University of Hawaii, we can still manage our business, be with our family, and earn the internationally recognized MBA degree. The program is one of the best choices in my life!”.</span></p>
+												<p class="text-primary text-right"><span><strong>Nguyen T. Tra My</strong><br></span>Founder, Chairwoman/CEO of<br>CSC Vietnam, Co-Founder/Vice<br>Chairwoman of The PAN Group.<br>Class of 2007 – Hanoi<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
 									<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
 									<div class="media-body">
-										<p class="text-primary"><span><strong>Nguyen T. Tra My</strong><br></span>Founder, Chairwoman/CEO of<br>CSC Vietnam, Co-Founder/Vice<br>Chairwoman of The PAN Group.<br>Class of 2007 – Hanoi<br></p>
 										<p class="text-muted"><span>“Why pursuing a quality MBA program overseas while it’s available in Vietnam? With the AACSB accredited Executive MBA program being offered by the University of Hawaii, we can still manage our business, be with our family, and earn the internationally recognized MBA degree. The program is one of the best choices in my life!”.</span></p>
+										<p class="text-primary"><span><strong>Nguyen T. Tra My</strong><br></span>Founder, Chairwoman/CEO of<br>CSC Vietnam, Co-Founder/Vice<br>Chairwoman of The PAN Group.<br>Class of 2007 – Hanoi<br></p>
+
 									</div>
 								</div>
 							</div>
 							<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Quyen Nguyen</strong><br></span>Manager,<br>Partner Operations (Google<br>Play Movies & YouTube), San<br>Francisco. Class of 2012 –<br>HCMC<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“There will be no difference once I have my MBA. I already feel different. It was the journey. And it has already changed me.”</span></p>
+												<p class="text-primary text-right"><span><strong>Quyen Nguyen</strong><br></span>Manager,<br>Partner Operations (Google<br>Play Movies & YouTube), San<br>Francisco. Class of 2012 –<br>HCMC<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
@@ -466,9 +474,10 @@
 							<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Luu Trung Thai</strong><br></span>CEO, Military Bank (MBB),<br>Class of 2009 – Hanoi<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“….The workload of the Vietnam Executive MBA program (VEMBA) is really heavy but worthwhile. During and after studying the program, I was assigned to different positions as Head of Retail Banking, IT, Securities and now the CEO of Military Bank. At each position, I always made full use of VEMBA knowledge and skills and delivered good results.”</span></p>
+												<p class="text-primary text-right"><span><strong>Luu Trung Thai</strong><br></span>CEO, Military Bank (MBB),<br>Class of 2009 – Hanoi<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
@@ -482,9 +491,10 @@
 							<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Chu Thi Thanh Ha</strong><br></span>Chairwoman, FPT Telecom<br>Class of 2005 – Hanoi<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“I am confident to take more critical responsibilities at my corporation as I have been trained in this EMBA program to think strategically and to have a sound understanding of the value chain of large organizations such as FPT Corp.”</span></p>
+												<p class="text-primary text-right"><span><strong>Chu Thi Thanh Ha</strong><br></span>Chairwoman, FPT Telecom<br>Class of 2005 – Hanoi<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
@@ -498,9 +508,10 @@
 							<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Dinh Duc Thang</strong><br></span>CEO, Opec Plastics., JSC<br>Class of 2013 – Hanoi<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“The two years pursuing the Executive MBA program of the University of Hawaii are the most energetic, valuable and memorable times of my life. I hold close to my heart all the memories of interesting courses, complicated case studies, strict professors with rigid deadlines, night-long group meetings with endless debates about the differentiation in business strategies, and stress-relieving gatherings with affectionate smiles and friendly faces. I am ready for a new journey ahead”</span></p>
+												<p class="text-primary text-right"><span><strong>Dinh Duc Thang</strong><br></span>CEO, Opec Plastics., JSC<br>Class of 2013 – Hanoi<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
@@ -514,9 +525,10 @@
 							<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Ha Thu Thanh</strong><br></span>CEO, Deloitte Vietnam<br>Class of 2003 – Hanoi<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“Education and learning business has always been of prime importance in my life. Being the CEO of Deloitte Vietnam, the largest auditing firm in Vietnam, it has indeed been a rewarding challenge for me to balance my work my family and the Executive MBA program with University of Hawaii. As I view this, I have improved on my skills and knowledge to work in a very global environment. The knowledge of professors combined with the experiences of my classmates, who happen to be not only Vietnamese but also many expatriates, has helped me manage my business more effectively & efficiently. The pursuit of an MBA requires lots of time and patience. If you have these, then don’t miss the chance to get a world class, well acclaimed, AACSB accredited MBA program right at your doorstep with the University of Hawaii at Manoa.”</span></p>
+												<p class="text-primary text-right"><span><strong>Ha Thu Thanh</strong><br></span>CEO, Deloitte Vietnam<br>Class of 2003 – Hanoi<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
@@ -530,9 +542,10 @@
 							<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Huynh Buu Quang</strong><br></span>CEO, Maritime Bank Vietnam<br>Class of 2011 – HCMC<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“My 2-year experience with the University of Hawaii’s Executive MBA program was undoubtedly one of the best periods in my life. It gave me chances to meet with wonderful people, both local and global, gain up-to-date and world class international best practices and know-how in various aspects of management techniques / philosophies and explore new things which I had never had the opportunity to do prior to joining this program. I could apply concepts and experiences acquired from the program into my daily job and life, almost instantly. The best part of all is the friendships that I developed during the 2-year journey which has extended well beyond completion of the program. It has more than paid off!”</span></p>
+												<p class="text-primary text-right"><span><strong>Huynh Buu Quang</strong><br></span>CEO, Maritime Bank Vietnam<br>Class of 2011 – HCMC<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
@@ -546,9 +559,10 @@
 							<div class="panel panel-info">
 								<div class="panel-heading clearfix d-none d-sm-block">
 											<img src="<?php echo $base_url;?>/assets/images/CroppedFocusedImageWyIyMDAiLCIyMDAiLCI0OS0zNiJd-mba1.jpg" class="media-left pull-left" alt="Tran Thien Tra">
-											<div class="media-body">
-												<p class="text-primary"><span><strong>Nguyen Anh Tuan</strong><br></span>Director, Transaction Banking, Standard Chartered Bank, Sydney<br>Class of 2011 – HCMC<br></p>
+											<div class="media-body contentStudent">
 												<p class="text-muted"><span>“The key success factors in pursuing an Executive MBA program are strong commitment, hard work, and relentless sacrifice of other interests. In return, this program has given me the required knowledge to succeed, strong entrepreneurship, much widened professional network, and wonderful relationships with faculty members and other alumni. Financially, the program has been a good investment for my career. Above all, it has amazingly built a different perspective in life – just like what Albert Einstein used to say “Try not to become a man of success, but rather try to become a man of value”. The Executive MBA at University of Hawaii was definitely a chapter wort highlighting.”</span></p>
+												<p class="text-primary text-right"><span><strong>Nguyen Anh Tuan</strong><br></span>Director, Transaction Banking, Standard Chartered Bank, Sydney<br>Class of 2011 – HCMC<br></p>
+
 											</div>
 								</div>
 								<div class="panel-heading clearfix d-block d-sm-none">
