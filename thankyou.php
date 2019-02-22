4285a85e3bb8456ca65f65600d3595d60820d441
Thanh Pham <thanhprodev@gmail.com>
2019-02-22 22:05:35 +0700

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,7 +1,7 @@
 <?php include('inc/header.php');?>
   <div class="jumbotron text-xs-center">
     <div class="container text-center">
-      <h1 class="display-3">THANK YOU !</h1>
+      <h1 class="display-3">Thank You!</h1>
       <p class="lead"><strong>Bạn đã đăng ký thành công</strong> chúng tôi sẽ liên hệ với bạn sớm</p>
       <p class="lead">
         <a class="btn btn-primary btn-sm" href="<?php echo $base_url?>" role="button">Trở về trang chủ</a>
