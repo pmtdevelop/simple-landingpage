c56c1260117f8965bd64df4754b93be6801e6f1d
pmtdevelop <pmtdevelop@gmail.com>
2019-02-22 18:09:46 +0700

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Thank you !!!</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+
+
+<section>
+
+</section>
+
+<h3>Cảm ơn bạn đã đăng ký</h3>
+<p><a href="#">Trở về trang chủ</a></p>
+
+</body>
+</html>
