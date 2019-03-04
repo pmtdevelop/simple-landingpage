46e89ee44d112bb3d435a671fbddba1609a627c1
trinhcongduc <38092290+trinhcongduc@users.noreply.github.com>
2019-03-04 20:54:19 +0700

    update send multi receiver

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,6 @@
 <?php
 // Config Global
-$base_url = "http://localhost/simple-landingpage";
+$base_url = "http://localhost:8888/simple-landingpage";
 // Config Seo
 $title = "Title here";
 $description = "Description";
@@ -26,7 +26,7 @@ $email_config->email_send = "vembaeduvn@gmail.com";
 $email_config->pass_send = "fjvkktizsaphnmri";   //  google app password
 
 // recipient
-$email_config->email_receive = "vembaeduvn@gmail.com";
+$email_config->email_receive = ["vembaeduvn@gmail.com","trinhcongduc1998@gmail.com"];
 
 
 $email_config->sender_name = "Vemba";   //  google app password
