e2d12aec9b5c74444759112679e68560b324c03b
trinhcongduc <38092290+trinhcongduc@users.noreply.github.com>
2019-03-22 21:06:00 +0700

    update

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!--Content page-->
 <?php include('inc/header.php');?>
 <?php
-
 $lang_default == "vi"?include('inc/content-home-vi.php'):include('inc/content-home-en.php')
-
 ?>
 <?php include('inc/footer.php');?>
